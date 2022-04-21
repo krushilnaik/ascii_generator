f9ce0047be4a9a38a4bf3eb0017553a4a77ea8bc
Krushil <krushilnaik96@gmail.com>
2022-04-21 19:16:29 -0400

    Move everything to root address

--- ascii_generator/settings.py
+++ ascii_generator/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'generator',
+    'ascii_generator',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
