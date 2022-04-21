8605828c5c78d7e7e8e1357f750aa4462b45e0f8
Krushil <krushilnaik96@gmail.com>
2022-04-21 12:49:11 -0400

    Configure main app route

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    response = "yes"
+
+    return render(request, "generator/home.html", {
+        "content": response
+    })
