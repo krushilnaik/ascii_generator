0bc0fba2d390f4d5c35f34a797bf8a1f1046e238
Krushil <krushilnaik96@gmail.com>
2022-04-21 23:12:59 -0400

    Styling tweaks

--- ascii_generator/forms.py
+++ ascii_generator/forms.py
@@ -2,4 +2,11 @@ from django import forms
 
 
 class ArtForm(forms.Form):
-    text = forms.CharField(label="", max_length=20, required=True)
+    text = forms.CharField(
+        label="",
+        max_length=15,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Enter something:'}
+        )
+    )
